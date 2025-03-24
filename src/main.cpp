99119ad1ba2a7e661f0dfb982abd034c5e4e84b8
thomas <safagot@lopers.unk.edu>
2025-03-24 16:10:29 -0500

    Add CMakeLists.txt, example main

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+// Includes
+#include <cstdio>
+#include <glm/glm.hpp>
+
+// Macros
+
+// Prototypes
+
+// Main method
+int main() {
+    auto myvec = glm::vec3();
+    printf("Hello, World!\n");
+}
+
+// Implementations
