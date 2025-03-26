2edb268e94d6cf44f2ef48d0ee5634a5d2fbf316
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2025-03-25 20:46:59 -0500

    Init Test file

---
+++ tests/Test.h
@@ -0,0 +1,32 @@
+//Includes
+
+//Camera
+#include <src/Camera/Camera.h>
+
+//Geometry
+#include "Geometry/HitRecord.h"
+#include "Geometry/Hittable.h"
+#include "Geometry/HittableList.h"
+#include "Geometry/Sphere.h"
+
+//Materials
+#include "Materials/Dielectric.h"
+#include "Materials/Lambertian.h"
+#include "Materials/Material.h"
+#include "Materials/Metal.h"
+
+//Renderer
+#include "Renderer/Raytracer.h"
+#include "Renderer/Renderer.h"
+#include "Renderer/RenderTarget.h"
+
+//Scene
+#include "Scene/Scene.h"
+
+//Utils
+#include "Utils/Headers.h"
+#include "Utils/Interval.h"
+#include "Utils/Ray.h"
+
+//Main
+#include "main.cpp"
