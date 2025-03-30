ca3c9feed42673539764d0c810dc9d59523a61cd
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2025-03-29 22:05:56 -0500

    add test material assertion

--- tests/Test.cpp
+++ tests/Test.cpp
@@ -1,170 +1,31 @@
-//Includes
+// Includes
+#include "../src/Materials/Metal.h"
 
-//Camera
-#include "Camera/Camera.h"
+// Macros
 
-//Geometry
-#include "Geometry/HitRecord.h"
-#include "Geometry/Hittable.h"
-#include "Geometry/HittableList.h"
-#include "Geometry/Sphere.h"
+// Structures
 
-//Materials
-#include "Materials/Dielectric.h"
-#include "Materials/Lambertian.h"
-#include "Materials/Material.h"
-#include "Materials/Metal.h"
+// Prototypes
+bool TestMaterial();
 
-//Renderer
-#include "Renderer/Raytracer.h"
-#include "Renderer/Renderer.h"
-#include "Renderer/RenderTarget.h"
-
-//Scene
-#include "Scene/Scene.h"
-
-//Utils
-#include "Utils/Headers.h"
-#include "Utils/Interval.h"
-#include "Utils/Ray.h"
-
-//Main
-#include "main.cpp"
-
-//Macros
-
-//Prototypes
-
-//Camera
-void TestCamera(void);
-
-//Geometry
-void TestGeometrySphere(void);
-
-//Materials
-void TestMaterialsDielectricPass(void);
-void TestMaterialsDielectricFail(void);
-
-void TestMaterialsLambertianPass(void);
-void TestMaterialsLabertianFail(void);
-
-void TestMaterialsMaterialPass(void);
-void TestMaterialsMaterialFail(void);
-
-void TestMaterialMetalPass(void);
-void TestMaterialMetalFail(void);
-
-//Renderer
-void TestRendererRaytracer(void);
-void TestRendererRenderer(void);
-void TestRendererRenderTarget(void);
-
-
-//Scene
-void TestScenePass(void);
-void TestSceneFail(void);
-
-//Utils
-void TestUtilsHeaders(void);
-
-
-// main Function
+// Main Function
 int main() {
-    // Test Camera
-    TestCamera();
-
-    // Test Geometry
-    TestGeometrySphere();
-    
-    // Test Materials
-    TestMaterialsDielectricPass();
-    TestMaterialsDielectricFail();
-
-    TestMaterialsLambertianPass();
-    TestMaterialsLabertianFail();
-
-    TestMaterialsMaterialPass();
-    TestMaterialsMaterialFail();
-
-    TestMaterialMetalPass();
-    TestMaterialMetalFail();
-
-    // Test Renderer
-    TestRendererRaytracer();
-
-    TestRendererRenderer();
-
-    TestRendererRenderTarget();
-
-    // Test Scene
-    TestScene();
-
-    // Test Utils
-    TestUtilsHeaders();
+    // Test Functions
+    TestMaterial();
 }
 
-//Camera
-void TestCamera() {
-    int width = 1280/2;
-    int height = 720/2;
-    if (RenderTarget(width, height)) {
-        printf(true);
-    } else {
-        prinf(false);
-    }
-}
+// Camera
+bool TestMaterial() {
+    bool test = true;
 
-//Geometry
-void TestGeometrySphere() {
-    auto material_green = make_shared<Lambertian>(dvec3(0.8, 0.8, 0.0));
-    if (Sphere(dvec3(0, 0, -100.5), 100, material_green)) {
-        printf(true);
-    } else {
-        prinf(false);
-    }
-}
+    try {
+        // Material
+        auto material_metal = make_shared<Metal>(dvec3(0.85), 0.01);
 
-//Material
-
-//Renderer
-void TestRendererRaytracer() {
-    if (Raytracer) {
-        printf(true);
-    } else {
-        printf(false);
+        //Insert assertion statement here
     }
-}
-
-void TestRendererRenderer() {
-    if (Renderer()) {
-        printf(true);
-    } else {
-        printf(false);
+    catch (const std::exception& e) {
+        test = false;
     }
-}
-
-void TestRendererRenderTargetPass() {
-    if (RenderTarget()) {
-        printf(true);
-    } else {
-        printf(false);
-    }
-}
-
-//Scene
-void TestScene() {
-    if Scene() {
-        printf(true);
-    } else {
-        printf(false);
-    }
-}
-
-//Utils
-void TestUtilsHeaders() {
-    if HEADERS_H {
-        printf(true);
-    } else {
-        printf(false);
-    }
+    return test; // Return test results
 }
