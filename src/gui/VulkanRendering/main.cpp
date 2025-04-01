d7ca8fb2842598fdeb022c149528ec2d48c745db
KadeSchrock <schrockkade@gmail.com>
2025-04-01 14:19:44 -0500

    Vulkan instance setup

--- src/gui/VulkanRendering/main.cpp
+++ src/gui/VulkanRendering/main.cpp
@@ -22,6 +22,8 @@ private:
 	//reference to "window" called in "initWindow()" function
 	GLFWwindow* window;
 
+	VkInstance instance;
+
 	//initialize glfw which is used to create windows
 	void initWindow() {
 		//initializes glfw library
@@ -37,6 +39,7 @@ private:
 	}
 	//initates vulkan, vulkan objects will be stored here as private class members and will have functions that call them
 	void initVulkan() {
+		createInstance();
 
 	}
 
@@ -50,13 +53,47 @@ private:
 
 	//deallocates resources once the window is closed
 	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
+
 		glfwDestroyWindow(window);
 
 		glfwTerminate();
 	}
+
+	//instance is connection between our application and the vulkan library
+	void createInstance() {
+		//information for driver to optimize our application
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		//tells Vulkan driver which global extensions and validation layers to use
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		//glfw function returns required extensions to interface with the window system
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		//determine which global validation layers to enable
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		//checks if instance was created successfully, if not created successfully throws error
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw::std::runtime_error("failed to create instance");
+		}
+	}
 };
 
-
+//runs app
 int main() {
 	HelloTriangleApplication app;
 
