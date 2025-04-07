aa32c6d0d6a66b8dac59f7bc72e12dcd4ce14a66
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2025-04-06 20:30:19 -0500

    Fixed Sphere List Tests

--- tests/TestSphereList.cpp
+++ tests/TestSphereList.cpp
@@ -6,13 +6,22 @@ namespace {
     void TestSphereList() {
         std::cout << "\t[SphereList] Testing SphereList..." << std::endl;
         auto s1 = make_shared<Sphere>(vec3(0, 0, 0), 1, Color(1, 1, 1));
-
+        auto ray = Ray(vec3(0, -1, 0), vec3(0, 1, 0));
+        auto hitRecord = HitRecord();
         const auto spheres = make_shared<SphereList>();
-
         spheres->Add(s1);
 
-    }
+        assert(spheres->Hit(ray, 0, 10000, hitRecord) == true);
 
+        try {
+            spheres->Hit(ray, -infinity, infinity, hitRecord);
+            assert(false);
+        } catch (SphereListException &e) {
+            assert(true);
+        } catch (...) {
+            assert(false);
+        }
+    }
 
     void TestSphereListAll() {
         std::cout << "[Unit Testing] Testing SphereList..." << std::endl;
